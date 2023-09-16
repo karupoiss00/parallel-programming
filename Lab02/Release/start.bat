6f8106523cd35c0aa0af8478ac4fc6c7f08f17fb
karupoiss00 <glizerin1108@gmail.com>
2023-09-16 14:47:34 +0300

    Допилил скриптец

--- Lab02/Release/start.bat
+++ Lab02/Release/start.bat
@@ -1,17 +1,4 @@
-echo off
-call bmb-blur.exe input.bmp output.bmp %1 1
-call bmb-blur.exe input.bmp output.bmp %1 2
-call bmb-blur.exe input.bmp output.bmp %1 3
-call bmb-blur.exe input.bmp output.bmp %1 4
-call bmb-blur.exe input.bmp output.bmp %1 5
-call bmb-blur.exe input.bmp output.bmp %1 6
-call bmb-blur.exe input.bmp output.bmp %1 7
-call bmb-blur.exe input.bmp output.bmp %1 8
-call bmb-blur.exe input.bmp output.bmp %1 9
-call bmb-blur.exe input.bmp output.bmp %1 10
-call bmb-blur.exe input.bmp output.bmp %1 11
-call bmb-blur.exe input.bmp output.bmp %1 12
-call bmb-blur.exe input.bmp output.bmp %1 13
-call bmb-blur.exe input.bmp output.bmp %1 14
-call bmb-blur.exe input.bmp output.bmp %1 15
-call bmb-blur.exe input.bmp output.bmp %1 16
+@echo off
+for /l %%x in (1, 1, 16) do (
+   call bmb-blur.exe input.bmp output.bmp %1 %%x
+)
